fab5d9fc3a949571850793874db293070e35a62c
Cramunhao23 <souzaluizfelipe37@outlook.com>
2023-03-10 00:57:35 -0300

    add componentes

---
+++ client/src/components/Cs.js
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+const Css = StyleSheet.create({
+
+});
+
+export default CSS;
---
+++ client/src/components/Maps.js
@@ -0,0 +1,11 @@
+import react from 'react' 
+import {Text, SafeAreaView} from 'react-native'
+
+export default props => {
+
+     return (
+<SafeAreaView>
+    <Text></Text>
+</SafeAreaView>
+     )
+}
