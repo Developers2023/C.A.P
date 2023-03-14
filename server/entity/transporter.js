9bda7294bb67e73cae2fb5eea036eaabd301357c
KevenFelixDaSilva <keve.05@hotmail.com>
2023-03-14 20:19:54 -0300

    modified entity

--- server/entity/transporter.js
+++ server/entity/transporter.js
@@ -1,19 +1,8 @@
 const baseClient = require('./baseClient');
 
 class transporter extends baseClient {
-  constructor() {
-    super(nome, sexo, email, cpf, celular, endereco, numCasa, cidade, cep, senha, cfSenha)
-    this.nome = nome;
-    this.sexo = sexo;
-    this.email = email;
-    this.cpf = cpf;
-    this.celular = celular;
-    this.endereco = endereco;
-    this.numCasa = numCasa;
-    this.cidade = cidade;
-    this.cep = cep;
-    this.senha = senha;
-    this.cfSenha = cfSenha;
+  constructor(nome, sexo, email, cpf, celular, endereco, numCasa, cidade, cep, senha) {
+    super(nome, sexo, email, cpf, celular, endereco, numCasa, cidade, cep, senha)
   }
 }
 
