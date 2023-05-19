85809a55332e60163827f5b888b34d0e6ba0b727
Cramunhao23 <souzaluizfelipe37@outlook.com>
2023-05-19 00:56:02 -0300

    luizteste

--- client/src/components/Cadastro.js
+++ client/src/components/Cadastro.js
@@ -38,12 +38,36 @@ const sighUpValidation = yup.object().shape({
 
 export default function Cadastro({navigation}) {
 
-const getCadastro = async () => {
-  const res = await api.get('../');
-  console.log(res)
+const cadastrarUsuario = async() => {
+  try {
+    const res = await api.post('/pessoa/cadastrar', {
+      nome,
+      email,
+      cpf,
+      tel,
+      endereco,
+      complementoDeEndereco,
+      cidade,
+      cep,
+      senha,
+      confirmarSenha,
+    });
+    console.log(res.data)
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-const [cadastro, setCadastro] = React.useState({})
+const [nome, setNome] = React.useState('')
+const [email, setEmail] = React.useState('')
+const [cpf, setCpf] = React.useState('')
+const [tel, setTel] = React.useState('')
+const [endereco, setEndereco] = React.useState('')
+const [complementoDeEndereco, setComplementoDeEndereco] = React.useState('')
+const [cidade, setCidade] = React.useState('')
+const [cep, setCep] = React.useState('')
+const [senha, setSenha] = React.useState('')
+const [confirmarSenha, setConfirmarSenha] = React.useState('')
 
 
 return(
@@ -62,7 +86,7 @@ return(
   }}
   validateOnMount={true}
   validationSchema={sighUpValidation}
-  onSubmit={values=> setCadastro(values)}
+  onSubmit={values=> console.log(values)}
 >
 {({handleSubmit,handleChange,handleBlur,values,touched,errors,isValid}) => (
   
@@ -85,14 +109,15 @@ return(
             <TextInput 
               style={[Css.inputs,Css.input_name]}
               name="nome"
-              onChangeText={handleChange('nome')}
-              onBlur={handleBlur('nome')}
-              value={values.nome}
+              onBlur={handleBlur('nome')}  
               placeholder='Nome:' placeholderTextColor={'#282B29'}
               inputMode='text'
               autoComplete='name'
               maxLength={100}
-              returnKeyType='next'/>
+              returnKeyType='next'
+              onChangeText={setNome}
+              value={nome}
+              />
             <Dropdown />
           </View>
           {(errors.nome && touched.nome) &&
@@ -101,13 +126,13 @@ return(
           
           <TextInput style={[Css.inputs,Css.inputs_all]}
             name="email"
-            onChangeText={handleChange('email')}
             onBlur={handleBlur('email')}
-            value={values.email}
             placeholder='E-mail:' placeholderTextColor={'#282B29'}
             inputMode='email' keyboardType='email-address'
             autoComplete='email'
             returnKeyType='next'
+            onChangeText={setEmail}
+            value={email}
             />
             {(errors.email && touched.email) &&
                   <Text style={Css.errors}>{errors.email}</Text>
@@ -116,13 +141,14 @@ return(
           <MaskedTextInput style={Css.mask} 
             name="cpf"
             mask='999.999.999-99'
-            onChangeText={handleChange('cpf')}
-            onBlur={handleBlur('cpf')}
-            value={values.cpf}
+            onBlur={handleBlur('cpf')}     
             placeholder='CPF:'
             placeholderTextColor={'#282B29'}
             returnKeyType='next'
-            keyboardType='numeric'/>
+            keyboardType='numeric'
+            value={cpf}
+            onChangeText={setCpf}
+            />
             {(errors.cpf && touched.cpf) &&
                   <Text style={Css.errors}>{errors.cpf}</Text>
                   }
@@ -130,38 +156,41 @@ return(
           <MaskedTextInput
           style={Css.mask}
             name="tel"
-            mask="(+99) 99 99999-9999"
-            onChangeText={handleChange('tel')}
-            onBlur={handleBlur('tel')}
-            value={values.tel} 
+            mask="(+99) 99 99999-9999"      
+            onBlur={handleBlur('tel')}   
             placeholder='Celular:'
             placeholderTextColor={'#282B29'}
             returnKeyType='next'
             autoComplete='tel'
-            keyboardType='numeric'/>
+            keyboardType='numeric'
+            value={tel} 
+            onChangeText={setTel}
+            />
             {(errors.tel && touched.tel) &&
               <Text style={Css.errors}>{errors.tel}</Text>
               }
 
           <View style={Css.view_input}>  
           <TextInput style={[Css.inputs,Css.input_address]}
-              name="endereco"
-              onChangeText={handleChange('endereco')}
-              onBlur={handleBlur('endereco')}
-              value={values.endereco}
+              name="endereco"        
+              onBlur={handleBlur('endereco')}            
               placeholder='Endereço:' placeholderTextColor={'#282B29'}
               autoComplete='street-address'
-              returnKeyType='next'/>
+              returnKeyType='next'
+              value={endereco}
+              onChangeText={setEndereco}
+              />
 
             <TextInput 
               style={[Css.inputs,Css.input_number]}
-              name="complementoDeEndereco"
-              onChangeText={handleChange('complementoDeEndereco')}
-              onBlur={handleBlur('complementoDeEndereco')}
-              value={values.complementoDeEndereco}
+              name="complementoDeEndereco"        
+              onBlur={handleBlur('complementoDeEndereco')} 
               placeholder='Nº:' placeholderTextColor={'#282B29'}
               inputMode='text'
-              returnKeyType='next'/>
+              returnKeyType='next'
+              value={complementoDeEndereco}
+              onChangeText={setComplementoDeEndereco}
+              />
               
           </View>
           {(errors.endereco && touched.endereco) &&
@@ -174,27 +203,29 @@ return(
       
           <View style={Css.view_input}>
               <TextInput 
-                name="cidade"
-                onChangeText={handleChange('cidade')}
-                onBlur={handleBlur('cidade')}
-                value={values.cidade}
+                name="cidade"                
+                onBlur={handleBlur('cidade')}               
                 style={[Css.inputs,Css.input_city]}
                 placeholder='Cidade:' placeholderTextColor={'#282B29'} inputMode='text'
                 returnKeyType='next'
-                maxLength={50}/>
+                maxLength={50}
+                value={cidade}
+                onChangeText={setCidade}
+                />
 
               <MaskedTextInput 
                 name="cep"
-                mask='99999-999'
-                onChangeText={handleChange('cep')}
-                onBlur={handleBlur('cep')}
-                value={values.cep}
+                mask='99999-999'               
+                onBlur={handleBlur('cep')}                
                 style={Css.mask_cep}
                 placeholder='CEP:' 
                 placeholderTextColor={'#282B29'}
                 autoComplete='postal-code'
                 keyboardType='numeric'
-                returnKeyType='next'/>
+                returnKeyType='next'
+                value={cep}
+                onChangeText={setCep}
+                />
               {/* 
                 inputMode='numeric' 
           maxLength={9} */}
@@ -210,26 +241,28 @@ return(
             }
     
           <TextInput style={[Css.inputs,Css.inputs_all]} 
-            name="senha"
-            onChangeText={handleChange('senha')}
-            onBlur={handleBlur('senha')}
-            value={values.senha}
+            name="senha"        
+            onBlur={handleBlur('senha')}    
             placeholder='Senha:' placeholderTextColor={'#282B29'} 
             inputMode='text'
             secureTextEntry={true}
-            returnKeyType='next'/>
+            returnKeyType='next'
+            onChangeText={setSenha}
+            value={senha}
+            />
             {(errors.senha && touched.senha) &&
                   <Text style={Css.errors_senha}>{errors.senha}</Text>
                   }
 
           <TextInput style={[Css.inputs,Css.inputs_all]}
-            name="confirmarSenha"
-            onChangeText={handleChange('confirmarSenha')}
-            onBlur={handleBlur('confirmarSenha')}
-            value={values.confirmarSenha}
+            name="confirmarSenha"        
+            onBlur={handleBlur('confirmarSenha')} 
             placeholder='Confirmar senha:' placeholderTextColor={'#282B29'} 
             inputMode='text'
-            secureTextEntry={true}/>
+            secureTextEntry={true}
+            onChangeText={setConfirmarSenha}
+            value={confirmarSenha}
+            />
             {(errors.confirmarSenha && touched.confirmarSenha) &&
                   <Text style={Css.errors_senha}>{errors.confirmarSenha}</Text>
                   }
@@ -238,7 +271,7 @@ return(
     
           <TouchableOpacity style={Css.btn_v1}
           onPress={()=>{
-            getCadastro();
+            cadastrarUsuario();
             handleSubmit();
             navigation.navigate('CadastrarCrianca')}
           }
