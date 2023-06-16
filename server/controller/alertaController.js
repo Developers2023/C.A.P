7a5e5c1de7f2ca6ac0a53bc299ec154dc8531221
Keven Félix <keven.05@hotmail.com>
2023-06-16 17:01:01 -0300

    desenvolvendo alertaController

--- server/controller/alertaController.js
+++ server/controller/alertaController.js
@@ -1,5 +1,9 @@
-module.exports = {
-    async mensagem(req,id){
+const Alertas = require("../entity/Alertas")
+const pessoaController = require("./pessoaController")
 
+module.exports = {
+    async alerta(body,id){
+        const destinatario = await pessoaController.find(body.code)
+        const response = new Alertas(body.atrasos,destinatario.id,id)
     }
 }
