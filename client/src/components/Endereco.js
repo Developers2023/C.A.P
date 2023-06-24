b14f25101f91cc299cf77a4dbd825f87fc8c0e1e
Cramunhao23 <souzaluizfelipe37@outlook.com>
2023-06-24 01:04:44 -0300

    luiz endereco correcao

--- client/src/components/Endereco.js
+++ client/src/components/Endereco.js
@@ -13,5 +13,75 @@ const enderecoSchema = yup.object().shape({
 })
 
 export default () => {
-    
+    <Formik
+        initialValues={{
+            Nome: '',
+            Sexo: '',
+            Deficiencia: '',
+            Instituicao: '',
+            Periodo: ''
+        }}
+        validateOnMount={true}
+        validationSchema={enderecoSchema}
+    >
+        {({ handleSubmit, handleChange, handleBlur, values, touched, errors, isValid }) => (
+            <SafeAreaView>
+                <View>
+
+                    <TextInput
+                        style={[Css.inputs, Css.inputs_all]}
+                        placeholder="Cidade:"
+                        placeholderTextColor={'#282B29'}
+                        inputMode="text"
+                        onBlur={handleBlur('cidade')}
+                        onChangeText={handleChange('Cidade')}
+                        value={values.cidade}
+                    />
+                    {(errors.cidade && touched.cidade) &&
+                        <Text style={Css.errors}>{errors.cidade}</Text>
+                    }
+
+                    <TextInput
+                        style={[Css.inputs, Css.inputs_all]}
+                        placeholder="Cep:"
+                        placeholderTextColor={'#282B29'}
+                        inputMode="numeric"
+                        onChangeText={handleChange('Cep')}
+                        onBlur={handleBlur('Cep')}
+                        value={values.cep}
+                    />
+                    {(errors.cep && touched.cep) &&
+                        <Text style={Css.errors}>{errors.cep}</Text>
+                    }
+
+                    <TextInput
+                        style={[Css.inputs, Css.inputs_all]}
+                        placeholder="Numero:"
+                        placeholderTextColor={'#282B29'}
+                        inputMode="numeric"
+                        onChangeText={handleChange('Numero')}
+                        onBlur={handleBlur('Numero')}
+                        value={values.numero}
+                    />
+                    {(errors.numero && touched.numero) &&
+                        <Text style={Css.errors}>{errors.numero}</Text>
+                    }
+
+                    <TextInput
+                        style={[Css.inputs, Css.inputs_all]}
+                        placeholder="Nome da rua"
+                        placeholderTextColor={'#282B29'}
+                        inputMode="text"
+                        onChangeText={handleChange('logradouro')}
+                        onBlur={handleBlur('logradouro')}
+                        value={values.logradouro}
+                    />
+                    {(errors.logradouro && touched.logradouro) &&
+                        <Text style={Css.errors}>{errors.logradouro}</Text>
+                    }
+
+                </View>
+            </SafeAreaView>
+        )}
+    </Formik>
 }
