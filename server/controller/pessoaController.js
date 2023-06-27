cadac8b8ec90d46f36c0bbab2a16ecac930ebaf5
Cramunhao23 <souzaluizfelipe37@outlook.com>
2023-06-27 01:58:30 -0300

    cadastro endereco

--- server/controller/pessoaController.js
+++ server/controller/pessoaController.js
@@ -7,31 +7,31 @@ const Sequelize = require('sequelize')
 module.exports = {
 
   async cadastrar(body) {
-    const pessoa = new Pessoa(body.tipo, body.nome, body.sexo, body.email, body.cpf, body.telefone, body.senha, null, null, body.nascimento) 
+    const pessoa = new Pessoa(body.tipo, body.nome, body.sexo, body.email, body.cpf, body.telefone, body.senha, null, null, body.nascimento)
     const usuario = await pessoaRepository.create(pessoa)
-    
-    /* const endereco = body.endereco
-    const enderecoEntity = new Endereco(endereco.logradouro, endereco.numero, endereco.cidade, endereco.cep,null,usuario.id)
-    await enderecoRepository.create(enderecoEntity) */
-    
+
+    const endereco = body.endereco
+    const enderecoEntity = new Endereco(endereco.logradouro, endereco.numero, endereco.cidade, endereco.cep, null, usuario.id)
+    await enderecoRepository.create(enderecoEntity)
+
     return usuario
   },
 
   async atualizar(body, id) {
     const pessoa = await pessoaRepository.update({
-      tipo:body.tipo,
-      nome:body.nome,
-      telefone:body.telefone,
-      sexo:body.sexo, 
-      email:body.email,
-      senha:body.senha,
-      nascimento:body.nascimento
-    },{where:{id:id}})
+      tipo: body.tipo,
+      nome: body.nome,
+      telefone: body.telefone,
+      sexo: body.sexo,
+      email: body.email,
+      senha: body.senha,
+      nascimento: body.nascimento
+    }, { where: { id: id } })
     return pessoa
   },
 
-  async deletar(id){
-    const result = await pessoaRepository.update({where: {id:id}},{cancelado: 1})
+  async deletar(id) {
+    const result = await pessoaRepository.update({ where: { id: id } }, { cancelado: 1 })
     return result
   },
 
@@ -40,8 +40,8 @@ module.exports = {
     let data = await pessoaRepository.findAll({
       where: {
         nome: { [Op.like]: `${body.nome}%` },
-        }
-      }   
+      }
+    }
     );
     return data
   }
