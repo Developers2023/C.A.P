4bd49f4a682bd91caab476bc671461c18d9d70be
Cramunhao23 <souzaluizfelipe37@outlook.com>
2023-06-27 17:41:24 -0300

    sla

--- client/App.js
+++ client/App.js
@@ -91,22 +91,19 @@ export default () => {
 
       <Stack.Navigator>
 
-        <Stack.Screen name='Mapa' component={Maps} />
+        <Stack.Screen name='Tab' component={Tabn} options={{ headerShown: false }} />
+        <Stack.Screen name='Entrada' component={Autentificacao} options={{headerShown: false}} />
         <Stack.Screen name='CadastroMo' component={CadastroMo} options={{headerShown: false}} />
         <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name='Entrada' component={Autentificacao} options={{headerShown: false}} />
         <Stack.Screen name='TelaInicial' component={TelaInicial} options={{ headerShown: false }} />    
-        {/* <Stack.Screen name='CadastroRe' component={CadastroRes} options={{headerShown: false}} /> */}
-        {/* <Stack.Screen name='Entrada' component={Autentificacao} options={{headerShown: false}} /> */}
         <Stack.Screen name='EnviarAlertas' component={EnviarAlertas} />
         <Stack.Screen name='Dados Pessoais' component={DadosPessoais} />
-        <Stack.Screen name='Tab' component={Tabn} options={{ headerShown: false }} />
-
+        {/* <Stack.Screen name='CadastroRe' component={CadastroRes} options={{headerShown: false}} /> */}
+        {/* <Stack.Screen name='Entrada' component={Autentificacao} options={{headerShown: false}} /> */}
         {/*NAO ACESSO */}
 
-
-        {/*COM  ACESSO */}
-       
+        {/*COM  ACESSO */}  
+        <Stack.Screen name='Mapa' component={Maps} />
         <Stack.Screen name='CopiarCod' component={CopiarID} />
         <Stack.Screen name='Lista de criancas' component={ListaCrianca} />
         <Stack.Screen name='CadastrarCrianca' component={CadastrarCrianca} />
