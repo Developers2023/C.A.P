bd362ec9b63fc9f0f3688b20db080ad0878b25f2
Keven Félix <keven.05@hotmail.com>
2023-06-27 17:26:56 -0300

    add login 200

--- client/App.js
+++ client/App.js
@@ -90,23 +90,17 @@ export default () => {
     <NavigationContainer>
 
       <Stack.Navigator>
+      
         <Stack.Screen name='TelaInicial' component={TelaInicial} options={{ headerShown: false }} />
         <Stack.Screen name='Login' component={Login} options={{ headerShown: false }} />
         <Stack.Screen name='CadastroMo' component={CadastroMo} options={{headerShown: false}} />
         {/* <Stack.Screen name='CadastroRe' component={CadastroRes} options={{headerShown: false}} /> */}
         {/* <Stack.Screen name='Entrada' component={Autentificacao} options={{headerShown: false}} /> */}
-        <Stack.Screen name='Mapa' component={Maps} />
+        <Stack.Screen name='Tab' component={Tabn} options={{ headerShown: false }} />
+        <Stack.Screen name='Maps' component={Maps} />
         <Stack.Screen name='EnviarAlertas' component={EnviarAlertas} />
         <Stack.Screen name='Dados Pessoais' component={DadosPessoais} />
-        <Stack.Screen name='Tab' component={Tabn} options={{ headerShown: false }} />
-
-        {/*NAO ACESSO */}
-        
-        
-
-
         {/*COM  ACESSO */}
-       
         <Stack.Screen name='CopiarCod' component={CopiarID} />
         <Stack.Screen name='Lista de criancas' component={ListaCrianca} />
         <Stack.Screen name='CadastrarCrianca' component={CadastrarCrianca} />
